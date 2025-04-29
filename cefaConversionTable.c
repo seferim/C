bbd4a6ef504d961b78a2001d7c7d10659bc39905
Panagiotis Filippakopoulos <panagiotis@ltp440s.home>
2025-04-29 17:00:10 +0300

    Refactor: replace of the while loop with for, for clarity/simplicity.

--- cefaConversionTable.c
+++ cefaConversionTable.c
@@ -2,20 +2,12 @@
 
 int main()
 {
-	float fahr, celsius, step;
-	int lower, upper;
-
-	lower = 0;
-	upper = 100;
+	float celsius, step;
+	
 	step = 15.5;
 
 	printf("Celsius to Fahrenheit conversion table\n");
 
-	celsius = lower;
-
-	while (celsius <= upper) {
-		fahr = celsius * (9.0/5.0) + 32;
-		printf("%3.2fC\t%6.2fF\n", celsius, fahr);
-		celsius += step;
-	}
+	for (celsius = 0; celsius <= 100; celsius += step)
+	printf("%3.2fC\t%6.2fF\n", celsius, (9.0/5.0) * celsius + 32);
 }
