28e2df8ea5cadcef57d62035fdbcf847bea3095e
Panagiotis Filippakopoulos <panagiotis@ltp440s.home>
2025-04-30 19:44:13 +0300

    Initial commit of a Fahrenheit to Celsius converter.

---
+++ fahrenheit-to-celsius-converter.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	int temperature;
+	float celsius;
+
+	printf("Enter temperature in Fahrenheit.\n");
+	scanf("%d", &temperature);
+
+	celsius = (5.0/9.0) * (temperature - 32.0);
+
+	printf("%d Fahrenheit is, %.2f Celius.", temperature, celsius);
+}	
